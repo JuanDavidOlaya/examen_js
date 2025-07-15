3ee617c1f6f19d434f5027005c6fc7c18c64dc6e
JuanDavidOlaya <juan123losdelsur@gmail.com>
2025-07-14 21:30:00 -0500

    Delete examen_olaya/spa_practice directory

--- examen_olaya/spa_practice/public/js/views/auth.js
+++
@@ -1,8 +0,0 @@
-export function isAuthenticated() {
-  return !!localStorage.getItem('session');
-}
-
-export function isAdmin() {
-  const session = JSON.parse(localStorage.getItem('session'));
-  return session?.role === 'admin';
-}
